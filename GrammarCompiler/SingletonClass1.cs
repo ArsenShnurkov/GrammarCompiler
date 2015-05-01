7b75990dd271906005f45d6c3f3e48d9fe0ed72d
Chris <cmagerabronn110@web.de>
2015-05-01 18:12:48 +0200

    test

---
+++ GrammarCompiler/SingletonClass1.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace GrammarCompiler
+{
+	/// <summary>
+	/// Description of SingletonClass1.
+	/// </summary>
+	public sealed class SingletonClass1
+	{
+		private static SingletonClass1 instance = new SingletonClass1();
+		
+		public static SingletonClass1 Instance {
+			get {
+				return instance;
+			}
+		}
+		
+		private SingletonClass1()
+		{
+		}
+	}
+}
